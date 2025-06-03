f7ba6db03df759e89ec8ca391b1df6f6880589bd
AmorimAntonio <amorimnetoo02@gmail.com>
2025-06-03 10:50:57 -0300

    doc: documentando o código

--- bot.py
+++ bot.py
@@ -6,42 +6,48 @@ from dotenv import load_dotenv
 from keep_alive import keep_alive
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN") #pegando o token do .env
 
 keep_alive() #manter o bot ativo
 
+
+# definindo as permissões do bot
 intents = discord.Intents.default()
 intents.message_content = True
 intents.reactions = True
 intents.members = True
 intents.guilds = True
 
-
+# definindo o prefixo do bot, nesse caso "!"
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# ID da mensagem que terá as reações
-ROLE_MESSAGE_ID = 1379108215450632347  # substitua pelo ID real da mensagem
+
+ROLE_MESSAGE_ID = 1379108215450632347 # ID da mensagem que com as reações
 EMOJI_ROLE_MAP = {
-    "🎲": 1365322123458908170,  # ID do cargo Fogo
-    "☕": 1379099812758163537,  # ID do cargo Gelo
+    "🎲": 1365322123458908170,  # ID do cargo 1
+    "☕": 1379099812758163537,  # ID do cargo 2
 }
 
 @bot.event
 async def on_ready():
-    print(f"Bot conectado como {bot.user}")
+    print(f"Bot conectado como {bot.user}") # printando no log quando o bot fica online 
 
 @bot.command()
-async def setup(ctx):
+async def setup(ctx): # quando o usuário digita "!setup"
+    # mensagem:
     msg = await ctx.send("Reaja para receber um cargo: 🎲 - Engenharia de dados, ☕ - Estágiario(a)")
-    await msg.add_reaction("🎲")
+    # emotes:
+    await msg.add_reaction("🎲") 
     await msg.add_reaction("☕")
+    # printando o id da mensagem enviada
     print(f"ID da mensagem: {msg.id}")
 
 @bot.event
-async def on_raw_reaction_add(payload):
+async def on_raw_reaction_add(payload): 
+    # verificand oquando alguém reage à mensagem
     if payload.message_id != ROLE_MESSAGE_ID or payload.user_id == bot.user.id:
         return
-
+    # ignorando as reações do próprio bot
     guild = bot.get_guild(payload.guild_id)
     if not guild:
         return
@@ -50,15 +56,16 @@ async def on_raw_reaction_add(payload):
     if not role_id:
         return
 
+    # pegando o cargo correspondente ao emote
     member = guild.get_member(payload.user_id)
     if member:
         role = guild.get_role(role_id)
         if role:
-            await member.add_roles(role)
+            await member.add_roles(role) # buscando o membro e adicionando o cargo
             print(f"Adicionado {role.name} para {member.display_name}")
 
 @bot.event
-async def on_raw_reaction_remove(payload):
+async def on_raw_reaction_remove(payload): # assim como o anterior, porém removendo o cargo
     if payload.message_id != ROLE_MESSAGE_ID or payload.user_id == bot.user.id:
         return
 
@@ -77,4 +84,4 @@ async def on_raw_reaction_remove(payload):
             await member.remove_roles(role)
             print(f"Removido {role.name} de {member.display_name}")
 
-bot.run(TOKEN)
+bot.run(TOKEN) # iniciando o bot
