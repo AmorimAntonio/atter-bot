577a202250a65407de75f1fe274d0ab3b9af6978
AmorimAntonio <amorimnetoo02@gmail.com>
2025-06-03 09:21:11 -0300

    feat: adicionando keep_alive

--- atter-bot/bot.py
+++ atter-bot/bot.py
@@ -3,8 +3,12 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from keep_alive import keep_alive
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+keep_alive() #manter o bot ativo
 
 intents = discord.Intents.default()
 intents.message_content = True
